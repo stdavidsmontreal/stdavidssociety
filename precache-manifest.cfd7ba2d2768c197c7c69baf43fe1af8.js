ea70c1e646f4e4a44cbca9d5047985ec1a5154d2
Rhys <rhyshalsey@gmail.com>
2019-03-07 20:07:11 -0500

    Updates

---
+++ precache-manifest.cfd7ba2d2768c197c7c69baf43fe1af8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9c9a57a2ee66aea8790d6707118cca2",
+    "url": "/stdavidssociety/static/media/welsh-dragon.a9c9a57a.svg"
+  },
+  {
+    "revision": "e596946672cc14b08a3a",
+    "url": "/stdavidssociety/static/js/runtime~main.e5969466.js"
+  },
+  {
+    "revision": "eb52b1309152fe1c7a12",
+    "url": "/stdavidssociety/static/js/main.eb52b130.chunk.js"
+  },
+  {
+    "revision": "6fbbb6bfec28056fe818",
+    "url": "/stdavidssociety/static/js/2.6fbbb6bf.chunk.js"
+  },
+  {
+    "revision": "eb52b1309152fe1c7a12",
+    "url": "/stdavidssociety/static/css/main.45608b57.chunk.css"
+  },
+  {
+    "revision": "f4734908effc0078f7af26bdb264120e",
+    "url": "/stdavidssociety/index.html"
+  }
+];
